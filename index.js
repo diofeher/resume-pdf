54bc5e10a06858e1181e2c7e48b77a94262594ec
Diogenes Fernandes <diofeher@gmail.com>
2024-07-22 13:10:21 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import { promises as fs } from 'fs'
+import * as theme from 'jsonresume-theme-elegant'
+import puppeteer from 'puppeteer'
+import { createServer } from 'http-server';
+import { render } from 'resumed'
+import { create } from 'domain';
+
+const resume = JSON.parse(await fs.readFile('resume.json', 'utf-8'));
+await render(resume, theme);
+
+const browser = await puppeteer.launch({headless: false});
+const page = await browser.newPage();
+
+var server = createServer({});
+
+server.listen("3000", "localhost", function () {});
+
+await page.goto("http://localhost:3000/resume.html", { waitUntil: 'networkidle0' })
+await page.pdf({ path: 'resume.pdf', format: 'a4', printBackground: true })
+await browser.close();
+server.close();
