d0c5266230e124d2caa62f3af92a29b6566ad843
Diogenes Fernandes <diofeher@gmail.com>
2024-07-22 14:23:50 -0300

    port 80 is denied

--- index.js
+++ index.js
@@ -11,16 +11,19 @@ await render(resume, theme);
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
+const download = async () => {
+    await page.goto(`http://${HOST}/resume.html`, { waitUntil: 'networkidle0' })
+    await page.pdf({ path: 'resume.pdf', format: 'a4', printBackground: true })
+    await browser.close();
+    server.close();
+}
+
 var server = createServer({});
 
 const HOST = "localhost";
-const PORT = 80;
+const PORT = 8080;
 
 server.listen(PORT, HOST, function () {
-    console.log("listening 8080");
+    console.log(`listening ${PORT}`);
+    download();
 });
-
-await page.goto(`http://${HOST}/resume.html`, { waitUntil: 'networkidle0' })
-await page.pdf({ path: 'resume.pdf', format: 'a4', printBackground: true })
-await browser.close();
-server.close();
